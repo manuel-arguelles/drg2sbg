4aee660f1304ecdc3143e62b82da3fced9e9ca4b
Manuel Argüelles <manuel.arguelles@gmail.com>
2012-07-20 22:22:39 -0500

    Fix license information
    Careless copy paste included foobar instead of program's name.

--- src/drgdata.c
+++ src/drgdata.c
@@ -8,13 +8,13 @@
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * Drg2sbg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Drg2sbg.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
